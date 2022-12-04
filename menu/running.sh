fc5b8b32741cfc4ba43db2f2ce6d05936c5bbf73
jhoker <jhokerhacker@gmail.com>
2022-12-05 03:22:11 +0700

    Update running.sh

--- menu/running.sh
+++ menu/running.sh
@@ -1,250 +1,158 @@
 #!/bin/bash
-GREEN='\033[0;32m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-NC='\033[0m'
-yl='\e[32;1m'
-bl='\e[36;1m'
-gl='\e[32;1m'
-rd='\e[31;1m'
-mg='\e[0;95m'
-blu='\e[34m'
-op='\e[35m'
-or='\033[1;33m'
-bd='\e[1m'
-color1='\e[031;1m'
-color2='\e[34;1m'
-color3='\e[0m'
-# Getting
-# IP Validation
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
+# Welcome Information
+# Edition : Stable Edition V1.0
+# Author  : Yth Bagoes
+# (C) Copyright 2021-2022 By Bagoes Vpn
+# =========================================
 
-MYIP=$(curl -sS ipinfo.io/ip)
+# // Exporting Language to UTF-8
+export LC_ALL='en_US.UTF-8'
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US.UTF-8'
+export LC_CTYPE='en_US.utf8'
 
-red='\e[1;31m'
-green='\e[1;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
+# // Export Banner Status Information
+export EROR="[${RED} EROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+# // Export Align
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
+# // Exporting URL Host
+export Server_URL="autosc.me/aio"
+export Server_Port="443"
+export Server_IP="underfined"
+export Script_Mode="Stable"
+export Auther="FsidVPN"
+export RED_BG='\e[41m'
+
+# // Exporting IP Address
+export IP=$( curl -s https://ipinfo.io/ip/ )
+# // OpenSSH
+openssh=$( systemctl status ssh | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $openssh == "running" ]]; then
+    status_openssh="${GREEN}Running${NC} ( No Eror )"
+else
+    status_openssh="${RED}No Running${NC} ( Eror )"
+fi
+
+# // Stunnel5
+stunnel5=$( systemctl status stunnel4 | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $stunnel5 == "running" ]]; then
+    status_stunnel5="${GREEN}Running${NC} ( No Eror )"
+else
+    status_stunnel5="${RED}No Running${NC} ( Eror )"
+fi
+
+# // Dropbear
+dropbear=$( systemctl status dropbear | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $dropbear == "running" ]]; then
+    status_dropbear="${GREEN}Running${NC} ( No Eror )"
+else
+    status_dropbear="${RED}No Running${NC} ( Eror )"
+fi
+
+# // Squid
+squid=$( systemctl status squid | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $squid == "running" ]]; then
+    status_squid="${GREEN}Running${NC} ( No Eror )"
+else
+    status_squid="${RED}No Running${NC} ( Eror )"
+fi
+
+# // SSH Websocket Proxy
+ssh_ws=$( systemctl status ws-stunnel | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $ssh_ws == "running" ]]; then
+    status_ws_epro="${GREEN}Running${NC} ( No Eror )"
+else
+    status_ws_epro="${RED}No Running${NC} ( Eror )"
+fi
+
+# // Trojan Proxy
+ss=$( systemctl status xray | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $ss == "running" ]]; then
+    status_ss="${GREEN}Running${NC} ( No Eror )"
+else
+    status_ss="${RED}No Running${NC} ( Eror )"
+fi
+
+# // NGINX
+nginx=$( systemctl status nginx | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+if [[ $nginx == "running" ]]; then
+    status_nginx="${GREEN}Running${NC} ( No Eror )"
+else
+    status_nginx="${RED}No Running${NC} ( Eror )"
+fi
+
+# =========================================================================================================
+
+# // Running Function Requirement
+os_detail
+script_version
+license_check
+
+if [[ $Bot == "1" ]]; then
+        bot='Allowed'
+else
+        bot='Not Allowed'
+fi
+
+if [[ $Beta == "1" ]]; then
+        beta='Allowed'
+else
+        beta='Not Allowed'
+fi
+
+if [[ $Backup == "1" ]]; then
+        backup='Allowed'
+else
+        backup='Not Allowed'
+fi
+
+# // Clear
 clear
+clear && clear && clear
+clear;clear;clear
 
-# GETTING OS INFORMATION
-source /etc/os-release
-Versi_OS=$VERSION
-ver=$VERSION_ID
-Tipe=$NAME
-URL_SUPPORT=$HOME_URL
-basedong=$ID
-
-# VPS ISP INFORMATION
-#ITAM='\033[0;30m'
-echo -e "$ITAM"
-REGION=$( curl -s ipinfo.io/region )
-#clear
-#COUNTRY=$( curl -s ipinfo.io/country )
-#clear
-#WAKTU=$( curl -s ipinfo.ip/timezone )
-#clear
-CITY=$( curl -s ipinfo.io/city )
-#clear
-#REGION=$( curl -s ipinfo.io/region )
-#clear
-
-# COLOR VALIDATION
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-clear
-
-# STATUS SERVICE OPENVPN
-if [[ $oovpn == "active" ]]; then
-  status_openvpn=" ${GREEN}Running ${NC}( No Error )"
-else
-  status_openvpn="${RED}  Not Running ${NC}  ( Error )"
-fi
-
-# STATUS SERVICE  SSH 
-if [[ $ssh_service == "running" ]]; then 
-   status_ssh=" ${GREEN}Running ${NC}( No Error )"
-else
-   status_ssh="${RED}  Not Running ${NC}  ( Error )"
-fi
-
-# STATUS SERVICE  SQUID 
-if [[ $squid_service == "running" ]]; then 
-   status_squid=" ${GREEN}Running ${NC}( No Error )"
-else
-   status_squid="${RED}  Not Running ${NC}  ( Error )"
-fi
-
-# STATUS SERVICE  VNSTAT 
-if [[ $vnstat_service == "running" ]]; then 
-   status_vnstat=" ${GREEN}Running ${NC}( No Error )"
-else
-   status_vnstat="${RED}  Not Running ${NC}  ( Error )"
-fi
-
-# STATUS SERVICE  CRONS 
-if [[ $cron_service == "running" ]]; then 
-   status_cron=" ${GREEN}Running ${NC}( No Error )"
-else
-   status_cron="${RED}  Not Running ${NC}  ( Error )"
-fi
-
-# STATUS SERVICE  FAIL2BAN 
-if [[ $fail2ban_service == "running" ]]; then 
-   status_fail2ban=" ${GREEN}Running ${NC}( No Error )"
-else
-   status_fail2ban="${RED}  Not Running ${NC}  ( Error )"
-fi
-
-# STATUS SERVICE  TLS 
-if [[ $tls_v2ray_status == "running" ]]; then 
-   status_tls_v2ray=" ${GREEN}Running${NC} ( No Error )"
-else
-   status_tls_v2ray="${RED}  Not Running${NC}   ( Error )"
-fi
-
-# STATUS SERVICE NON TLS V2RAY
-if [[ $nontls_v2ray_status == "running" ]]; then 
-   status_nontls_v2ray=" ${GREEN}Running ${NC}( No Error )${NC}"
-else
-   status_nontls_v2ray="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
-
-# STATUS SERVICE VLESS HTTPS
-if [[ $vless_tls_v2ray_status == "running" ]]; then
-  status_tls_vless=" ${GREEN}Running${NC} ( No Error )"
-else
-  status_tls_vless="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
-
-# STATUS SERVICE VLESS HTTP
-if [[ $vless_nontls_v2ray_status == "running" ]]; then
-  status_nontls_vless=" ${GREEN}Running${NC} ( No Error )"
-else
-  status_nontls_vless="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
-# STATUS SERVICE TROJAN
-if [[ $trojan_server == "running" ]]; then 
-   status_virus_trojan=" ${GREEN}Running ${NC}( No Error )${NC}"
-else
-   status_virus_trojan="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
-# Status Service Shadowsocks
-if [[ $sssws == "active" ]]; then
-  status_ssws=" ${GREEN}Running ${NC}( No Error )${NC}"
-else
-  status_trgo="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
-# STATUS SERVICE DROPBEAR
-if [[ $dropbear_status == "running" ]]; then 
-   status_beruangjatuh=" ${GREEN}Running${NC} ( No Error )${NC}"
-else
-   status_beruangjatuh="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
-
-# STATUS SERVICE STUNNEL
-if [[ $stunnel_service == "running" ]]; then 
-   status_stunnel=" ${GREEN}Running ${NC}( No Error )"
-else
-   status_stunnel="${RED}  Not Running ${NC}  ( Error )}"
-fi
-# STATUS SERVICE WEBSOCKET TLS
-if [[ $wstls == "running" ]]; then 
-   swstls=" ${GREEN}Running ${NC}( No Error )${NC}"
-else
-   swstls="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
-
-# STATUS SERVICE WEBSOCKET DROPBEAR
-if [[ $wsdrop == "running" ]]; then 
-   swsdrop=" ${GREEN}Running ${NC}( No Error )${NC}"
-else
-   swsdrop="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
-
-
-
-# TOTAL RAM
-total_ram=` grep "MemTotal: " /proc/meminfo | awk '{ print $2}'`
-totalram=$(($total_ram/1024))
-
-# TIPE PROCESSOR
-#totalcore="$(grep -c "^processor" /proc/cpuinfo)" 
-#totalcore+=" Core"
-#corediilik="$(grep -c "^processor" /proc/cpuinfo)" 
-#tipeprosesor="$(awk -F ': | @' '/model name|Processor|^cpu model|chip type|^cpu type/ {
-  #                      printf $2;
-      #                  exit
-    #                    }' /proc/cpuinfo)"
-
-# GETTING CPU INFORMATION
-#cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
-#cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
-#cpu_usage+=" %"
-
-# OS UPTIME
-#uptime="$(uptime -p | cut -d " " -f 2-10)"
-
-# KERNEL TERBARU
-kernelku=$(uname -r)
-
-# WAKTU SEKARANG 
-#harini=`date -d "0 days" +"%d-%m-%Y"`
-#jam=`date -d "0 days" +"%X"`
-
-# DNS PATCH
-#tipeos2=$(uname -m)
-Name=$(curl -sS https://raw.githubusercontent.com/sibeesans/ajsc/main/anjay/allow | grep $MYIP | awk '{print $2}')
-Exp=$(curl -sS https://raw.githubusercontent.com/sibeesans/ajsc/main/anjay/allow | grep $MYIP | awk '{print $3}')
-# GETTING DOMAIN NAME
-Domen="$(cat /etc/xray/domain)"
-echo -e ""
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m              ⇱ Sytem Information ⇲             \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "❇️ Hostname    : $HOSTNAME"
-echo -e "❇️ OS Name     : $Tipe"
-# echo -e "Processor   : $tipeprosesor"
-# echo -e "Proc Core   :$totalcore"
-# echo -e "Virtual     :$typevps"
-# echo -e "Cpu Usage   :$cpu_usage"
-echo -e "❇️ Total RAM   : ${totalram}MB"
-echo -e "❇️ Public IP   : $MYIP"
-echo -e "❇️ Domain      : $Domen"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m          ⇱ Subscription Information ⇲          \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "❇️ Client Name : $Name"
-echo -e "❇️ Exp Script  : $Exp"
-echo -e "❇️ Version     : Latest Version"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m            ⇱ Service Information ⇲             \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "❇️ SSH / TUN               :$status_ssh"
-echo -e "❇️ OpenVPN                 :$status_openvpn"
-echo -e "❇️ Dropbear                :$status_beruangjatuh"
-echo -e "❇️ Stunnel4                :$status_stunnel"
-echo -e "❇️ Squid                   :$status_squid"
-echo -e "❇️ Fail2Ban                :$status_fail2ban"
-echo -e "❇️ Crons                   :$status_cron"
-echo -e "❇️ Vnstat                  :$status_vnstat"
-echo -e "❇️ XRAYS Vmess TLS         :$status_tls_v2ray"
-echo -e "❇️ XRAYS Vmess None TLS    :$status_nontls_v2ray"
-echo -e "❇️ XRAYS Vless TLS         :$status_tls_vless"
-echo -e "❇️ XRAYS Vless None TLS    :$status_nontls_vless"
-echo -e "❇️ XRAYS Trojan            :$status_virus_trojan"
-echo -e "❇️ Shadowsocks             :$status_ssws"
-echo -e "❇️ Websocket TLS           :$swstls"
-echo -e "❇️ Websocket None TLS      :$swsdrop"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "${YELLOW}----------------------------------------------------------${NC}"
+echo -e "                ${YELLOW}(${NC}${GREEN} STATUS SERVICE INFORMATION ${NC}${YELLOW})${NC}"
+echo -e "         OWNER : ${GREEN}bagoes vpn ${NC}${YELLOW}(${NC} ${GREEN}bagoesvpn ${NC}${YELLOW})${NC}"
+echo -e "       © Copyright By bagoes vpn ${YELLOW}(${NC} 2021-2022 ${YELLOW})${NC}"
+echo -e "${YELLOW}----------------------------------------------------------${NC}"
 echo ""
+echo -e "${RED_BG}                     Sytem Information                    ${NC}"
+echo -e "Sever Uptime        = $( uptime -p  | cut -d " " -f 2-10000 ) "
+echo -e "Current Time        = $( date -d "0 days" +"%d-%m-%Y | %X" )"
+echo ""
+
+echo -e "${RED_BG}                     Service Information                  ${NC}"
+echo -e "OpenSSH             = $status_openssh"
+echo -e "Dropbear            = $status_dropbear"
+echo -e "Stunnel5            = $status_stunnel5"
+echo -e "Squid               = $status_squid"
+echo -e "NGINX               = $status_nginx"
+echo -e "SSH NonTLS          = $status_ws_epro"
+echo -e "SSH TLS             = $status_ws_epro"
+echo -e "Vmess WS/GRPC       = $status_ss"
+echo -e "Vless WS/GRPC       = $status_ss"
+echo -e "Trojan WS/GRPC      = $status_ss"
+echo -e "Shadowsocks WS/GRPC = $status_ss"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
