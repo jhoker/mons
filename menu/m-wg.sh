450996bd2767de2a4bf56076fb5efc14254a5226
jhoker <jhokerhacker@gmail.com>
2022-12-06 06:58:38 +0700

    Update m-wg.sh

--- menu/m-wg.sh
+++ menu/m-wg.sh
@@ -5,9 +5,8 @@ echo -e "\E[0;100;33m       • WireGuard Menu •         \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Create WireGuard Account "
-echo -e " [\e[36m•2\e[0m] Check WireGuard Account "
-echo -e " [\e[36m•3\e[0m] Renew WireGuard Account "
-echo -e " [\e[36m•4\e[0m] Delete WireGuard Account "
+echo -e " [\e[36m•2\e[0m] Renew WireGuard Account "
+echo -e " [\e[36m•3\e[0m] Delete WireGuard Account "
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e ""
@@ -19,9 +18,8 @@ read -p " Select menu :  "  opt
 echo -e ""
 case $opt in
 1) clear ; add-wg ; exit ;;
-2) clear ; cek-wg ; exit ;;
-3) clear ; renew-wg ; exit ;;
-4) clear ; del-wg ; exit ;;
+2) clear ; renew-wg ; exit ;;
+3) clear ; del-wg ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; m-wg ;;
