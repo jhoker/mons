36e50f3cfd79e66ee315e670979e68b54938d371
jhoker <jhokerhacker@gmail.com>
2022-12-05 11:35:08 +0700

    Update about.sh

--- menu/about.sh
+++ menu/about.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
-echo -e " \E[0;41;36m                 Bagoes Vpn Service                \E[0m"
+echo -e " \E[0;41;36m                 Kemem Vpn Service                \E[0m"
 echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
