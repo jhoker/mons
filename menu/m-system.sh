9712283d429ad5ec82bf116a89affaf5e0b17654
jhoker <jhokerhacker@gmail.com>
2022-12-06 10:28:45 +0700

    Update m-system.sh

--- menu/m-system.sh
+++ menu/m-system.sh
@@ -13,6 +13,7 @@ echo -e " [\e[36m•6\e[0m] Set Auto Reboot"
 echo -e " [\e[36m•7\e[0m] Restart All Service"
 echo -e " [\e[36m•8\e[0m] Change Banner"
 echo -e " [\e[36m•9\e[0m] Cek Bandwith"
+echo -e " [\e[36m•10\e[0m] Limit Speed"
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e   ""
@@ -32,6 +33,7 @@ case $opt in
 7) clear ; restart ; exit ;;
 8) clear ; nano /etc/issue.net ; exit ;; #ssh-vpn banner.conf
 9) clear ; bw ; exit ;;
+10) clear ; limit-speed ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; m-system ;;
